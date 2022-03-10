001fc61bd7b4265de1d9aa669ddb17087a44801a
Radu15 <100381429+rraadduu@users.noreply.github.com>
2022-03-10 20:48:45 +0000

    import

---
+++ demos/import.py
@@ -0,0 +1,5 @@
+import call_function 
+# import call_function as bob - use to rename
+# from call_function import calc_area - use to import a specific function
+# from call_+function import calc_area as bob - use to rename function
+print(f"Area of triangle is {call_function.calc_area(10, 2)} ")
