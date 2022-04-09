4b18a8037ba7f6862502bc5b98a14afdb46ed7ef
Radu15 <100381429+rraadduu@users.noreply.github.com>
2022-04-09 10:20:56 +0000

    w7tasks

--- w7/plottask2.py
+++ w7/plottask2.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 def small():
   x = [3, 3, 4, 4, 3]
   y = [3, 4, 4, 3, 3]
-  plt.plot(x, y, 'r:o')
+  plt.plot(x, y, 'r:o')#r=red, :=doted line, o=corner dots
   
 def medium():
   x = [2, 2, 5, 5, 2]
   y = [2, 5, 5, 2, 2]
-  plt.plot(x, y, 'g--s')
+  plt.plot(x, y, 'g--s')#g=green, --=two dash lines
   
 def large():
   x = [1, 1, 6, 6, 1]
   y = [1, 6, 6, 1, 1]
-  plt.plot(x, y, 'b-p')
+  plt.plot(x, y, 'b-p')#b=blue, -=one line, p=penthagon
   
 def run():
   small()
